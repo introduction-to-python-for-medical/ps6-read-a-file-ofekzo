c9593d1a4b1ed6452ad352f8d87f14b5f3492f0d
ofekzo <ofekzohar25@gmail.com>
2024-12-21 22:10:26 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,11 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    dicofco={}
+    for r in rows[0:]:
+      
+      cells=r.strip().split('\t')
+      key = cells[0]
+      value = cells[2]
+      dicofco[key] = value
+    return dicofco
 
 
