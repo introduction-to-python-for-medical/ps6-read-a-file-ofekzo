76267b44e41124d2e8c2186920c6c06bfe8172e7
ofekzo <ofekzohar25@gmail.com>
2025-01-26 13:41:45 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -6,7 +6,7 @@ def create_codon_dict(file_path):
         rows = file.readlines()
     
     # Loop through each row (starting from 1 if you want to skip header)
-    for r in rows[1:]:  # Skipping the first row if it's a header
+    for r in rows[0:]:  # Skipping the first row if it's a header
         cells = r.strip().split('\t')  # Split by tab to get columns
         key = cells[0]  # Assuming the key is in the first column
         value = cells[2]  # Assuming the value is in the third column
